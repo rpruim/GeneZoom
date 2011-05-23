10c94e59ba15ad0d21d87f7053b5aa973477e62a
Randall Pruim <rpruim@gmail.com>
2011-05-23 11:33:20 -0400

    added histogram.py stub in PlayPen

---
+++ PlayPen/histogram.py
@@ -0,0 +1,9 @@
+
+######################################################################################
+#
+# a program to read in a csv file with headers and produce a histogram of one of the 
+# columns.
+#
+# usage histogram -file somefile.csv -c column_name
+#
+######################################################################################

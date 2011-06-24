e99f07f233eab8af82f8cd276b9639b08b86f652
Jakeniah Christiansen <jcc7@students.calvin.edu>
2011-06-24 16:53:59 -0400

    Almost supports png and pdf

--- src/dotPlot.py
+++ src/dotPlot.py
@@ -4,11 +4,11 @@ Created on Jun 8, 2011
 A program to generate a dotgraph based on information acquired from a vcf file, using genezoom to create a table of said data
 @author: jcc7
 '''
-import vcf
-#from vcfutils import *
 import re
 import CrossTable
-import matplotlib.pyplot as plt
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pylab as plt
 from matplotlib.patches import Circle, Rectangle, Ellipse
 from matplotlib.lines import Line2D
 from matplotlib.collections import PatchCollection
@@ -18,7 +18,6 @@ import bed
 ######################################################
 
 #Method to convert a base pair to its location as a base pair in the exon
-#Returns -1 if the gene is not inside an exon
 def bp2exonbp(tupleList, number):
     sum=0
     #whether it should be 0-based or 1-based
@@ -26,20 +25,33 @@ def bp2exonbp(tupleList, number):
     number=int(number)
     for entry in tupleList:
         if (number>=entry[0])&(number<entry[1]):
-            return sum+number+1-entry[0]+base-1
+            return sum+number-entry[0]+base
         elif (number)<entry[0]:
-            return -1
+            return sum-1
         else:
             sum=sum+entry[1]-entry[0]
+    return sum +1
+
+#receives the list of tuples, returns a dictionary with the exon bp locations accessed by keys pertaining to their base pair
+def exonbplist(tupleList):
+    exonDict={}
+    location=0
+    for entry in tupleList:
+        for i in range(entry[0], entry[1]):
+            exonDict[i]= location
+            location +=1
+    return exonDict
 
 #draw a exon at the proper location
 def drawExon(exonTupleList):
     #Rectangle (xpos (left), ypos (bottom), width, height, kwargs)
-    patches=[Rectangle((exonTupleList[0][0], -1), 1, 2, fill=False)]
+    patches=[Rectangle((0, -1), 1, 2, fill=False)]
+    loc=0
     for exon in exonTupleList:
-        start=exon[0]
-        end=exon[1]
-        patches.append(Rectangle((start, -1), end-start-1, 2))
+        start=loc
+        width=exon[1]-exon[0]-1
+        patches.append(Rectangle((start, -1), width, 2))
+        loc=loc+width+1
     return PatchCollection(patches, match_original=True)
 
 #draw stacked circles in a number equal to circles
@@ -81,19 +93,19 @@ def dotPlot(stuff, xLoc):
             circleLoc=multiCircles(patches, oneCircles, xLoc, circleLoc, circleWidth, circleHeight, colorShade)
     return PatchCollection(patches, match_original=True)
 
-def SetupPlot(start, end):
+def SetupPlot(start, end, ymin, ymax):
         #set up the graph format
         fig=plt.figure(facecolor='white')
         #add axes in rectangle left position, bottom position, width, height
         ax1 = fig.add_axes([0.1, 0.3, 0.8, 0.6])
         ax2=fig.add_axes([0.1, 0.1, 0.8, 0.2], sharex=ax1)
         ax2.set_yticks([])
-        ax1.set_title("Frequency of Alleles")
+        ax1.set_title(options.title)
         ax1.set_ylabel("case                         control")
         ax2.set_xlabel("Chromosome")
         halfrange=(end-start)/2.0
         center=start+halfrange
-        ax1.axis([center-halfrange, center+halfrange, -7, 7])
+        ax1.axis([center-halfrange, center+halfrange, ymin, ymax])
         horizontalLine=Line2D([-1, 100000000000], [0, 0],linewidth=1, color='black')
         ax1.add_line(horizontalLine)
         ax1.grid(True)
@@ -101,7 +113,7 @@ def SetupPlot(start, end):
         ax2.add_line(horizontalLine)
         #fig.subplots_adjust(bottom=0.2)
 
-        return ax1, ax2
+        return ax1, ax2, fig
     
 
 ############################################################
@@ -184,7 +196,45 @@ if __name__ == "__main__":
         help ="prefix for output files", 
         metavar="STRING"
         )
-
+    parser.add_option(
+        "--title",
+        dest="title",
+        default="Frequency of alleles",
+        help="desired title for the plot"
+        )
+    parser.add_option(
+        "--png",
+        dest="png",
+        default=False,
+        help="Save graph to png"
+        )
+    parser.add_option(
+        "--pdf",
+        dest="pdf",
+        default=False,
+        help="Save graph to pdf"
+        )
+#    parser.add_option(
+#        "--nograph",
+#        dest="graph",
+#        default=True,
+#        action="store_false",
+#        help="Show the plot in an interactive session"
+#        )
+#    parser.add_option(
+#        "--graph",
+#        dest="graph",
+#        default=True,
+#        action="store_true",
+#        help="Show the plot in an interactive session"
+#        )
+    parser.add_option(
+        "-y", "--yscale", 
+        dest="yscale", 
+        default='-7:7',
+        help="y scale of graphed plot", 
+        metavar="ymin:ymax"
+        )
     (options, args) = parser.parse_args()
 
     #Begin regular expression compile for chrom, start, and stop
@@ -194,11 +244,18 @@ if __name__ == "__main__":
     chrom=int(m.groups()[0])
     start=int(m.groups()[1])
     end=int(m.groups()[2])
+    
+#    scaleRE=re.compile(r'(^[-+]?[0-9]*):(^[-+]?[0-9]*)')
+#    
+#    s=scaleRE.match(options.yscale)
+#    ymin=int(s.groups()[0])
+#    ymax=int(s.groups()[1])
     try:
         from vcfutils import *        
         v=vcfReader(options.vcf_file)
         vstuff = v.query(chrom, start, end)
     except Exception as e:
+        import vcf
         print e
         v = vcf.VCFdata(options.vcf_file, 1024*1024)
         vstuff = v.fetch_range(chrom, start, end)
@@ -207,19 +264,23 @@ if __name__ == "__main__":
     refFlat = bed.BED(options.bed, keys=refFlatKeys)
     bedRow=[row for row in refFlat if row['name']==options.gene ][0]
     
-    #check for dimensions of graph, and to see if the introns intersect with the desired region
-    ax1, ax2=SetupPlot(start, end)
+    exonDict=exonbplist(bedRow.get_exons())
+    if not exonDict.has_key(start):
+        start=bp2exonbp(bedRow.get_exons(), start)
+    else:
+        start=exonDict[start]
+    if not exonDict.has_key(end):
+        end=bp2exonbp(bedRow.get_exons(), end)
+    else:
+        end=exonDict[end]
+    ax1, ax2, fig=SetupPlot(start, end, -7, 7)
     
-    xLoc=start
-
-
     #for each element of vstuff (the data of chromosomes) create the cross table, add the proper dotGraph to the total plot
-    
     for v in vstuff:
         #check to see if the gene is in the exon
-        if not bp2exonbp(bedRow.get_exons(), v.getpos())==-1:
+        if exonDict.has_key(int(v.getpos())):
             xTable=CrossTable.xTable(options.trait_file, v.genotypes())
-            dots=dotPlot(xTable, v.getpos())
+            dots=dotPlot(xTable, exonDict[int(v.getpos())])
             ax1.add_collection(dots)
     
     exonRect=drawExon(bedRow.get_exons())
@@ -227,6 +288,10 @@ if __name__ == "__main__":
     ax2.add_line(Line2D([-1, 100000000000], [0, 0],linewidth=1, color='black'))
     ax1.add_line(Line2D([-1, 100000000000], [0, 0],linewidth=1, color='black'))
     #ax1.set_xticks([xtickgathered])
-    plt.show()
-
+    if True:
+        plt.show()
+    if options.png:
+        plt.savefig(options.prefix+'.png')
+    if options.pdf:
+        fig.savefig(options.prefix+'.pdf')
 ##################################################################################

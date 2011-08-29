eff8e0905de8182721f3bd480faa7ca208b01a60
Jakeniah Christiansen <jcc7@students.calvin.edu>
2011-08-29 15:37:40 -0400

    tests altered

--- src/genebatch.bat
+++ src/genebatch.bat
@@ -7,9 +7,7 @@
 
 #Testing different gene
 --bed "../testing/data/refFlat.txt.gz.1" -v "../testing/data/458_samples_from_bcm_bi_and_washu.annot.vcf.gz.1" -t "../testing/data/458_traits.csv" -g "T2D" --gene SCRIB -p results/newgene --title "New gene, default filter" --nograph --png
-#Testing user-provided colors and dimensions
---bed "../testing/data/refFlat.txt.gz.1" -v "../testing/data/458_samples_from_bcm_bi_and_washu.annot.vcf.gz.1" -t "../testing/data/458_traits.csv" -g "T2D" --gene SAMD11 -p results/diffcolors --title "Different colors, dimensions" --nograph --png --color "#00ff00,#ff0000,#0000ff" --dimensions "12,8"
-#Testing flanking amount and introns
---bed "../testing/data/refFlat.txt.gz.1" -v "../testing/data/458_samples_from_bcm_bi_and_washu.annot.vcf.gz.1" -t "../testing/data/458_traits.csv" -g "T2D" --gene SAMD11 -p results/flanking --title "Flanking & introns test" -f 200 --nograph --png --introns
+#Testing user-provided colors, dimensions, introns
+--bed "../testing/data/refFlat.txt.gz.1" -v "../testing/data/458_samples_from_bcm_bi_and_washu.annot.vcf.gz.1" -t "../testing/data/458_traits.csv" -g "T2D" --gene SAMD11 -p results/diffcolors --title "Different colors, dimensions, introns" --nograph --png --color "#00ff00,#ff0000,#0000ff" --dimensions "12,8" --nointrons
 #Testing codons, exoncolor, nolegend
 --bed "../testing/data/refFlat.txt.gz.1" -v "../testing/data/458_samples_from_bcm_bi_and_washu.annot.vcf.gz.1" -t "../testing/data/458_traits.csv" -g "T2D" --gene SAMD11 -p results/testing --title "codons, exoncolor, nolegend" --nograph --png --exoncolor "#00ff00,#ff0000" --nolegend --codons

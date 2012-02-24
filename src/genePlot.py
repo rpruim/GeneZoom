92d4bf60df07f8461317f797f15443f8c921cf04
Randall Pruim <rpruim@gmail.com>
2012-02-24 10:57:12 -0500

    added comment about turing 'Agg' on or off

--- src/genePlot.py
+++ src/genePlot.py
@@ -5,7 +5,7 @@ A program to generate a dotgraph based on information acquired from a vcf file, 
 
 '''
 import matplotlib
-matplotlib.use("Agg")
+matplotlib.use("Agg")   # need to add code to decide whether or not to do this.
 import CrossTable
 import os
 import matplotlib.pyplot as plt
